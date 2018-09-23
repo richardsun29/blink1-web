295fd7be8a77c6cba854a66db09b9284e2083435
Richard Sun <richardsun29@gmail.com>
2018-09-23 00:09:29 -0700

    blink main

--- src/blink/index.ts
+++ src/blink/index.ts
@@ -1,6 +1,19 @@
 import MessageReceiver from '../util/message-receiver';
+import Blink from './blink';
 
-const m = new MessageReceiver();
-m.bind('my-event', (data) => {
-  console.log(data);
-});
+class BlinkApp {
+  private messageReceiver: MessageReceiver;
+  private blink: Blink;
+
+  constructor() {
+    this.messageReceiver = new MessageReceiver();
+    this.blink = new Blink();
+  }
+
+  public run(): void {
+    this.messageReceiver.bind('blink', this.blink.processMessage);
+  }
+}
+
+const app = new BlinkApp();
+app.run();
