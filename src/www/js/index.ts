e8ce0dab3eb6474a24f8c4c93accc2a7dbe047d2
Richard Sun <richardsun29@gmail.com>
2018-09-30 03:00:15 -0700

    handle spectrum clear

--- src/www/js/index.ts
+++ src/www/js/index.ts
@@ -3,10 +3,14 @@ declare var jQuery: typeof import('jquery');
 declare var _: typeof import('lodash');
 declare var tinycolor: typeof import('tinycolor2');
 
-import Api from './api-service';
+import ApiService from './api-service';
 
 const onColorSelect = (color: tinycolor.Instance) => {
-  Api.blinkSetColor(color);
+  if (color === null) {
+    ApiService.blinkOff();
+    return;
+  }
+  ApiService.blinkSetColor(color);
 };
 
 export function EntryPoint(): void {
