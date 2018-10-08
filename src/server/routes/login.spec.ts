4fd7345954a9616a6f6d886bfd17a3c6fae5dbd6
Richard Sun <richardsun29@gmail.com>
2018-10-07 23:54:00 -0700

    login unit tests

---
+++ src/server/routes/login.spec.ts
@@ -0,0 +1,117 @@
+import { strict as assert } from 'assert';
+import cookieParser from 'cookie-parser';
+import express from 'express';
+import sinon from 'sinon';
+import request from 'supertest';
+
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+import Config from '../../util/config';
+
+import login from './login';
+
+describe('Login', () => {
+  const password = 'password123';
+  const jwtSecret = 'jwt secret';
+  const fakeMainPage = '<h1>Main page</h1>';
+
+  const app: express.Application = express();
+  app.use(express.json());
+  app.use(cookieParser());
+  app.use(login);
+  app.get('/', (req, res, next) => res.send(fakeMainPage));
+
+  beforeEach(() => {
+    sinon.replaceGetter(Config, 'PASSWORD_HASH', () => bcrypt.hashSync(password, 10));
+    sinon.replaceGetter(Config, 'JWT_SECRET', () => jwtSecret);
+  });
+
+  afterEach(() => {
+    sinon.verifyAndRestore();
+  });
+
+  function assertIsLoginPage(done: Mocha.Done): request.CallbackHandler {
+    return (err, res) => {
+      if (err) {
+        done(err);
+      } else {
+        assert.notStrictEqual(res.text.indexOf('<h1>Login</h1>'), -1);
+        done();
+      }
+    };
+  }
+  function assertIsMainPage(done: Mocha.Done): request.CallbackHandler {
+    return (err, res) => {
+      if (err) {
+        done(err);
+      } else {
+        assert.strictEqual(res.text, fakeMainPage);
+        done();
+      }
+    };
+  }
+
+  describe('GET', () => {
+    it('should show login page if not logged in', (done) => {
+      request(app).get('/')
+        .expect(403)
+        .end(assertIsLoginPage(done));
+    });
+
+    it('should handle invalid cookie', (done) => {
+      request(app).get('/')
+        .set('Cookie', 'jwt=invalid')
+        .expect(403)
+        .end(assertIsLoginPage(done));
+    });
+
+    it('should handle unverified cookie', (done) => {
+      const badToken = jwt.sign({}, 'wrong secret');
+      request(app).get('/')
+        .set('Cookie', `jwt=${badToken}`)
+        .expect(403)
+        .end(assertIsLoginPage(done));
+    });
+
+    it('should redirect to main page if logged in', (done) => {
+      const token = jwt.sign({}, jwtSecret);
+      request(app).get('/')
+        .set('Cookie', `jwt=${token}`)
+        .expect(200)
+        .end(assertIsMainPage(done));
+    });
+  });
+
+  describe('POST', () => {
+    it('should handle no password', (done) => {
+      request(app).post('/')
+        .expect(400)
+        .end(assertIsLoginPage(done));
+    });
+
+    it('should reject incorrect password', (done) => {
+      request(app).post('/')
+        .send({ password: 'wrong password' })
+        .expect(403)
+        .end(assertIsLoginPage(done));
+    });
+
+    it('should accept correct password', (done) => {
+      const agent = request.agent(app);
+
+      agent.post('/')
+        .send({ password })
+        .expect(302)
+        .expect('Location', '/')
+        .end((err, res) => {
+          if (err) {
+            done(err);
+          }
+          agent.get('/')
+            .expect(200)
+            .end(assertIsMainPage(done));
+        });
+    });
+  });
+});
--- src/server/routes/login.ts
+++ src/server/routes/login.ts
@@ -3,18 +3,31 @@ const router: express.Router = express.Router();
 
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-import path from 'path';
 
 import Config from '../../util/config';
 
-const loginPage: string = path.resolve('dist', 'www', 'login.html');
+const loginPage: string = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>blink(1) web</title>
+  </head>
+  <body>
+    <h1>Login</h1>
+    <form method="POST">
+      <input name="password" type="password" placeholder="Password">
+      <button type="submit">Submit</button>
+    </form>
+  </body>
+</html>
+`;
 
 router.get('/', (req, res, next) => {
   const token = req.cookies.jwt;
   jwt.verify(token, Config.JWT_SECRET, (err: any, decoded: any) => {
     if (err) {
       res.status(403);
-      res.sendFile(loginPage);
+      res.send(loginPage);
     } else {
       next();
     }
@@ -24,18 +37,18 @@ router.get('/', (req, res, next) => {
 router.post('/', express.urlencoded({ extended: true }), (req, res, next) => {
   if (!req.body.password) {
     res.status(400);
-    res.sendFile(loginPage);
+    res.send(loginPage);
+    return;
   }
 
   bcrypt.compare(req.body.password, Config.PASSWORD_HASH, (err, same) => {
     if (same) {
       const token = jwt.sign({}, Config.JWT_SECRET);
       res.cookie('jwt', token);
-      res.status(200);
       res.redirect('/');
     } else {
       res.status(403);
-      res.sendFile(loginPage);
+      res.send(loginPage);
     }
   });
 });
